43613f55655955dc66a2957d86cbdca645dbde54
Jacob Mish <Jacobpmish@gmail.com>
2025-04-16 15:41:52 -0600

    Applied cargo fmt & clippy

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,7 @@ use event::run_app;
 use persistence::save_transactions;
 
 use crossterm::{
-    event::{EnableBracketedPaste, DisableBracketedPaste},
+    event::{DisableBracketedPaste, EnableBracketedPaste},
     terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
     ExecutableCommand,
 };
@@ -20,7 +20,9 @@ use std::result::Result as StdResult;
 
 fn main() -> StdResult<(), Box<dyn std::error::Error>> {
     enable_raw_mode()?;
-    stdout().execute(EnterAlternateScreen)?.execute(EnableBracketedPaste)?;
+    stdout()
+        .execute(EnterAlternateScreen)?
+        .execute(EnableBracketedPaste)?;
     let mut terminal = Terminal::new(CrosstermBackend::new(stdout()))?;
 
     let mut app = App::new();
